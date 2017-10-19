895b745d00885014380e2f7f38a2d3278d8a39a8
DIEGO CAMPOS DIANEZ <dcampos@bravent.net>
2017-10-19 13:52:55 +0200

    Add solution to repository

---
+++ HtmlToPdfSample/Program.cs
@@ -0,0 +1,68 @@
+﻿using DinkToPdf;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HtmlToPdfSample
+{
+    class Program
+    {
+        static SynchronizedConverter converter;
+        public static void Main(string[] args)
+        {
+            converter = new SynchronizedConverter(new PdfTools());
+
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Grayscale,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Margins = new MarginSettings() { Top = 10 },
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        Page = "http://www.color-hex.com/"
+                    }
+                }
+            };
+
+            Action(doc);
+
+            var doc2 = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    PaperSize = PaperKind.A4Small
+                },
+
+                Objects = {
+                    new ObjectSettings()
+                    {
+                        Page = "http://google.com/",
+                    }
+                }
+            };
+
+
+            Action(doc2);
+
+            Console.ReadKey();
+        }
+
+        private static void Action(HtmlToPdfDocument doc)
+        {
+            byte[] pdf = converter.Convert(doc);
+
+            if (!Directory.Exists("Files"))
+            {
+                Directory.CreateDirectory("Files");
+            }
+
+            using (FileStream stream = new FileStream(@"Files\" + DateTime.UtcNow.Ticks.ToString() + ".pdf", FileMode.Create))
+            {
+                stream.Write(pdf, 0, pdf.Length);
+            }
+        }
+    }
+}
+
---
+++ WebHtmlToPdfSample/Controllers/ConvertController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DinkToPdf.Contracts;
+using DinkToPdf;
+
+namespace WebHtmlToPdfSample.Controllers
+{
+    [Route("api/Convert")]
+    public class ConvertController : Controller
+    {
+        private IConverter _converter;
+
+        public ConvertController(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    PaperSize = PaperKind.A4Plus,
+                    Orientation = Orientation.Portrait,
+                },
+
+                Objects = {
+                    new ObjectSettings()
+                    {
+                        PagesCount=true,
+                         FooterSettings = {  Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 },
+                        Page = "http://google.com/",
+                    },
+                     new ObjectSettings()
+                    {
+                        PagesCount=true,
+                         FooterSettings = {  Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 },
+                        Page = "http://www.color-hex.com/",
+
+                    }
+                }
+            };
+
+            byte[] pdf = _converter.Convert(doc);
+
+
+            return new FileContentResult(pdf, "application/pdf");
+        }
+    }
+}
